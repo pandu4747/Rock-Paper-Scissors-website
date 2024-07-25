55af212479a2e7409968ddebcb2ad92bc962d2e9
A Tarakeshwar <85379090+pandu4747@users.noreply.github.com>
2024-07-25 21:07:45 +0530

    Update rock-paper-scissor-game.js

--- rock-paper-scissor-game.js
+++ rock-paper-scissor-game.js
@@ -105,11 +105,11 @@ function selectcompMove() {
   let compMove = "";
 
   if (randomNum >= 0 && randomNum < 1 / 3) {
-    compMove = "ROCK";
+    compMove = "rock";
   } else if (randomNum >= 1 / 3 && randomNum < 2 / 3) {
-    compMove = "PAPER";
+    compMove = "paper";
   } else if (randomNum >= 2 / 3 && randomNum < 1) {
-    compMove = "SCISSORS";
+    compMove = "scissors";
   }
   console.log(compMove);
 
